6bab1cb0d3ad4e30ecfa55c48435611e28f1f721
Olivoz <lucas.sivars@gmail.com>
2022-10-11 10:54:33 +0200

    Implement load button

---
+++ public/js/app.js
@@ -0,0 +1,34 @@
+let contacts = [];
+
+function appendElement(target, elementName, content, id) {
+  let element = document.createElement(elementName);
+  if (content) element.innerText = content;
+  if (id) element.id = id;
+  target.appendChild(element);
+  return element;
+}
+
+function appendContact(contact) {
+  const tableEntry = appendElement(contactsTableElement, "tr");
+  appendElement(tableEntry, "td", contact.name);
+  appendElement(tableEntry, "td", contact.phone);
+  appendElement(tableEntry, "td", contact.email);
+
+  const delButton = appendElement(tableEntry, "button", "Del");
+  const editButton = appendElement(tableEntry, "button", "Edit");
+}
+
+function loadContacts() {
+  fetch("/contacts")
+    .then((res) => res.json())
+    .then((json) => {
+      contacts = json.contacts;
+      displayContacts();
+    });
+}
+
+function displayContacts() {
+  contacts.forEach((contact) => {
+    appendContact(contact);
+  });
+}
